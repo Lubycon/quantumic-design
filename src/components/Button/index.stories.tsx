aeb575f58f24f87341626782832e33a06870f843
JunKim <93kimhyunjun@gmail.com>
2020-11-16 23:48:18 +0900

    chore: delete no use story code

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import Button from './index';
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Meta } from '@storybook/react/types-6-0';
 
 export default {
   title: 'Lubycon UI Kit/Button',
   component: Button,
-  // argTypes: {},
 } as Meta;
 
-export const Default: React.FunctionComponent = () => <Button>기본 버튼</Button>;
-
-// const Template: Story = (args) => <Button {...args}>기본 버튼</Button>;
-
-// export const Default = Template.bind({});
-
-// Default.args = {};
+export const Default: React.FC = () => <Button>기본 버튼</Button>;
