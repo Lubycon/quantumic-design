5258114826f8d7344731c907d29b0251f77825f0
evan-moon <bboydart91@gmail.com>
2020-11-28 00:26:13 +0900

    chore(ui-kit): storybook deploy test

--- ui-kit/.storybook/deploy.js
+++ ui-kit/.storybook/deploy.js
@@ -9,12 +9,11 @@ ghpages.publish(path.join(__dirname, '../storybook-static'), {
   remote: 'origin',
   repo: `https://${token}@github.com/Lubycon/lubycon-ui-kit-storybook.git`,
   message: `개발용 스토리북 배포`,
-  callback: (err) => {
-    if (err) {
-      console.log('😢 배포에 실패하였습니다.');
-      console.error(err);
-    } else {
-      console.log('🚀 개발용 스토리북 배포가 완료되었습니다!')
-    }
+}, (err) => {
+  if (err) {
+    console.log('😢 배포에 실패하였습니다.');
+    console.error(err);
+  } else {
+    console.log('🚀 개발용 스토리북 배포가 완료되었습니다!')
   }
 });
