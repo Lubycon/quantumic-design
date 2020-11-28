1c7f777d7df8f1affad7d5de50a65e8b46a62a44
Evan Moon <bboydart91@gmail.com>
2020-11-28 21:25:37 +0900

    feat(ui-kit): Text 컴포넌트 추가 (#18)
    * chore: Alpha, Beta 브랜치 개설 [skip ci] (#14)
    
    * chore: 배포 테스트
    
    * chore: 배포테스트
    
    * chore: 배포 테스트
    
    * chore: dry run test
    
    * feat(ui-kit): Text 컴포넌트 타이핑에 as 프로퍼티 추가
    
    * 스토리북 세팅
    
    * chore(ui-kit): Text 스토리 추가
    
    * feat(ui-kit): Text에 Font Weight 스코어가 제대로 안 먹는 현상 수정
    
    * chore(ui-kit): remove console log
    
    * chore(ui-kit): 스토리북 배포
    
    * chore(ui-kit): fix workflow syntax
    
    * chore(ui-kit): action trigger test
    
    * chore(ui-kit): action trigger test
    
    * chore(ui-kit): 스토리북 배포 테스트
    
    * chore(ui-kit): 스토리북 배포 테스트
    
    * chore(ui-kit): 테스트 트리거 추가
    
    * chore(ui-kit): storybook deploy test
    
    * chore(ui-kit): test storybook deploy
    
    * chore(ui-kit): 스토리북 배포 액션을 메뉴얼로 수정

---
+++ ui-kit/.storybook/preview.js
@@ -0,0 +1,6 @@
+import '../src/sass/index.scss';
+import React from 'react';
+
+export const decorators = [(Story => (
+  <Story />
+))]
---
+++ ui-kit/src/components/Text/index.tsx
@@ -0,0 +1,31 @@
+import React, { ElementType, Ref } from 'react';
+import { DEFAULT_ELEMENT, FontWeights, Typographys } from './types';
+import { forwardRef } from 'react';
+import { OverridableProps } from 'types/OverridableProps';
+import clxs from 'classnames';
+
+interface TextBaseProps {
+  typography?: Typographys;
+  fontWeight?: FontWeights;
+}
+type TextProps<T extends ElementType = typeof DEFAULT_ELEMENT> = OverridableProps<T, TextBaseProps>;
+
+const Text = <T extends ElementType = typeof DEFAULT_ELEMENT>(
+  { typography = 'content', fontWeight, as, ...props }: TextProps<T>,
+  ref: Ref<HTMLButtonElement>
+) => {
+  const target = as ?? DEFAULT_ELEMENT;
+  const Component = target;
+  return (
+    <Component
+      ref={ref}
+      className={clxs('lubycon-text', {
+        [`lubycon-typography--${typography}`]: typography != null,
+        [`lubycon-text--font-weight--${fontWeight}`]: fontWeight != null,
+      })}
+      {...props}
+    />
+  );
+};
+
+export default forwardRef(Text) as typeof Text;
