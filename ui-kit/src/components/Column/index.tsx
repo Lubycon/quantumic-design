604d145e1f9305aaae8c1552283e912e3b3511d4
JaewonKwon <springkjw@gmail.com>
2020-12-06 03:02:32 +0900

    feat(ui-kit): Column 컴포넌트 Props

--- ui-kit/src/components/Column/index.tsx
+++ ui-kit/src/components/Column/index.tsx
@@ -1,9 +1,56 @@
+import classNames from 'classnames';
 import React, { HTMLAttributes } from 'react';
 
+type NumberAttribute =
+  | number
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11'
+  | '12';
+type ColumnSize = boolean | 'auto' | NumberAttribute;
+
 interface ColumnProps extends HTMLAttributes<HTMLDivElement> {
-  gutters: boolean;
+  xs?: ColumnSize;
+  sm?: ColumnSize;
+  md?: ColumnSize;
+  lg?: ColumnSize;
+  xl?: ColumnSize;
 }
 
-export default function Column({ gutters = false, ...props }: ColumnProps): JSX.Element {
-  return <div>{props.children}</div>;
+const sizes = ['xl' as const, 'lg' as const, 'md' as const, 'sm' as const, 'xs' as const];
+
+export default function Column({ children, ...props }: ColumnProps): JSX.Element {
+  const classes: string[] = [];
+
+  sizes.forEach(function (size) {
+    const propValue = props[size];
+    delete props[size];
+
+    const span: ColumnSize | undefined = propValue;
+    let offset: NumberAttribute | undefined;
+
+    const infix = size !== 'xs' ? `-${size}` : '';
+
+    if (span) {
+      classes.push(span ? `col${infix}` : `col${infix}-${span}`);
+    }
+
+    if (offset) {
+      classes.push(`offset${infix}-${offset}`);
+    }
+  });
+
+  return (
+    <div {...props} className={classNames(...classes)}>
+      {children}
+    </div>
+  );
 }
