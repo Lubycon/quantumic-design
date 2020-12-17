2ddfa645f802e8ff60f43bf45e89c1b0c5ca15fc
JaewonKwon <springkjw@gmail.com>
2020-12-18 01:20:14 +0900

    feat(ui-kit): Column 내부 Immutable하게 변경

--- ui-kit/src/components/Column/index.tsx
+++ ui-kit/src/components/Column/index.tsx
@@ -1,50 +1,57 @@
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, useState } from 'react';
 
+import { DEFAULT_ELEMENT } from './types';
+import { OverridableProps } from 'types/OverridableProps';
 import classNames from 'classnames';
 
 type NumberAttribute = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 type ColumnSize = boolean | 'auto' | NumberAttribute;
 
-interface ColumnProps extends HTMLAttributes<HTMLDivElement> {
+interface ColumnBaseProps extends HTMLAttributes<HTMLDivElement> {
   xs?: ColumnSize;
   sm?: ColumnSize;
   md?: ColumnSize;
   lg?: ColumnSize;
   xl?: ColumnSize;
 }
+type ColumnProps<T extends React.ElementType = typeof DEFAULT_ELEMENT> = OverridableProps<
+  T,
+  ColumnBaseProps
+>;
 
 type ColumnResponsive = 'xl' | 'lg' | 'md' | 'sm' | 'xs';
 const sizes: ColumnResponsive[] = ['xl', 'lg', 'md', 'sm', 'xs'];
 
-export default function Column({ children, ...props }: ColumnProps): JSX.Element {
-  const spans: string[] = [];
-  const classes: string[] = [];
+const Column = <T extends React.ElementType = typeof DEFAULT_ELEMENT>(
+  { as, ...props }: ColumnProps<T>,
+  ref: React.Ref<any>
+) => {
+  const [spans, setSpans] = useState<string[]>([]);
 
   sizes.forEach(function (size) {
-    const propValue = props[size];
-    delete props[size];
+    const { [size]: propValue } = props;
 
-    const span: ColumnSize | undefined = propValue;
-    let offset: NumberAttribute | undefined;
+    if (!propValue) {
+      return;
+    }
 
+    const { span } = propValue;
     const infix = size !== 'xs' ? `-${size}` : '';
 
     if (span) {
-      spans.push(span ? `column${infix}-${span}` : `column${infix}`);
-    }
-
-    if (offset) {
-      classes.push(`offset${infix}-${offset}`);
+      const currentSpan = span ? `column${infix}-${span}` : `column${infix}`;
+      setSpans((prevSpans) => [...prevSpans, currentSpan]);
     }
   });
 
-  if (!spans.length) {
+  if (spans.length === 0) {
     spans.push('column');
   }
 
-  return (
-    <div {...props} className={classNames(...spans, ...classes)}>
-      {children}
-    </div>
-  );
-}
+  const target = as ?? DEFAULT_ELEMENT;
+  const Component = target;
+
+  return <Component ref={ref} className={classNames(...spans)} {...props} />;
+};
+
+export default React.forwardRef(Column) as typeof Column;
---
+++ ui-kit/src/components/Column/types.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_ELEMENT = 'div' as const;
