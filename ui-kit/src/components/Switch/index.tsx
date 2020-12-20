e83846e55e16a972ca974e4d4bd59e78bbaca4a0
JunKim <93kimhyunjun@gmail.com>
2020-12-21 00:29:03 +0900

    feat(ui-kit): label 추가

--- ui-kit/src/components/Switch/index.tsx
+++ ui-kit/src/components/Switch/index.tsx
@@ -3,19 +3,29 @@ import { Ref } from 'react';
 import { CombineElementProps } from 'src/types/utils';
 import clxs from 'classnames';
 import { generateID } from 'src/utils/generateID';
+import { Text } from '..';
 
 interface SwitchBaseProps {
   label?: string;
+  display?: 'block' | 'inline';
 }
 type SwitchProps = Omit<CombineElementProps<'input', SwitchBaseProps>, 'type'>;
 
-const Switch = ({ style, ...props }: SwitchProps, ref: Ref<HTMLInputElement>) => {
+const Switch = (
+  { label, display = 'block', style, ...props }: SwitchProps,
+  ref: Ref<HTMLInputElement>
+) => {
   const id = generateID('switch');
 
   return (
-    <label role="switch" className={clxs('lubycon-switch')} style={style}>
+    <label
+      role="switch"
+      className={clxs('lubycon-switch', `lubycon-switch--display-${display}`)}
+      style={style}
+    >
       <input className="lubycon-switch--input" ref={ref} type="checkbox" {...props} id={id} />
       <span className="lubycon-switch--slider" />
+      {label ? <Text as="span">{label}</Text> : null}
     </label>
   );
 };
