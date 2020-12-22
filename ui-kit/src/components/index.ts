b8ade40a43b384b52fa22c48aeaddb656ed0c0fe
evan-moon <bboydart91@gmail.com>
2020-12-22 15:44:37 +0900

    fix(ui-kit): remove duplicated export syntax

--- ui-kit/src/components/index.ts
+++ ui-kit/src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from './Button';
 export { default as Text } from './Text';
 export { Row, Column } from './Grid';
-export { default as Column } from './Column';
 export { default as Checkbox } from './Checkbox';
