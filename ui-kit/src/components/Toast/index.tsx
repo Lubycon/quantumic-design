59e2e8d2f667ea25554b4923b3c3f9ce9df7777e
evan-moon <bboydart91@gmail.com>
2021-01-17 20:03:35 +0900

    feat(ui-kit): 토스트 스토리 업데이트

--- ui-kit/src/components/Toast/index.tsx
+++ ui-kit/src/components/Toast/index.tsx
@@ -1,52 +1,77 @@
-import React, { HTMLAttributes } from 'react';
-import { animated, useSpring } from 'react-spring';
+import React, { HTMLAttributes, useEffect, useState } from 'react';
+import { animated, useTransition } from 'react-spring';
 import classnames from 'classnames';
-import { Portal } from 'contexts/Portal';
 import ToastBody from './ToastBody';
 
 export interface ToastProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
   show: boolean;
   message: string;
-  duration?: number;
+  autoHideDuration?: number;
   onShow?: () => void;
   onHide?: () => void;
 }
-const Toast = ({ show, message, onShow, onHide, className, style, ...rest }: ToastProps) => {
-  const animation = useSpring<{
-    progress: number;
-    opacity: number;
-    translateY: number;
-    onFrame: (props: { progress: number }) => void;
-  }>({
-    progress: show ? 1 : 0,
-    opacity: show ? 1 : 0,
-    translateY: show ? 0 : 100,
-    onFrame: ({ progress }) => {
-      if (show && progress === 1) {
-        onShow?.();
-      }
+const Toast = ({
+  show,
+  message,
+  autoHideDuration,
+  onShow,
+  onHide,
+  className,
+  style,
+  ...rest
+}: ToastProps) => {
+  const [isOpen, setOpen] = useState(show);
 
-      if (!show && progress === 0) {
-        onHide?.();
-      }
+  const transition = useTransition(isOpen, null, {
+    from: {
+      opacity: 0,
+      transform: 'translateY(100%)',
+    },
+    enter: {
+      opacity: 1,
+      transform: 'translateY(0)',
+    },
+    leave: {
+      opacity: 0,
+      transform: 'translateY(-100%)',
+    },
+    onStart: () => {
+      onShow?.();
+    },
+    onDestroyed: () => {
+      onHide?.();
     },
   });
 
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (autoHideDuration != null && isOpen === true) {
+      timer = setTimeout(() => {
+        setOpen(false);
+      }, autoHideDuration);
+    }
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Portal>
-      <animated.div
-        className={classnames('lubycon-toast', className)}
-        style={{
-          ...style,
-          display: animation.progress.interpolate((v) => (v === 0 ? 'none' : 'block')),
-          opacity: animation.opacity.interpolate((v) => v),
-          transform: animation.translateY.interpolate((v) => `translateY(${v}%)`),
-        }}
-        {...rest}
-      >
-        <ToastBody message={message} />
-      </animated.div>
-    </Portal>
+    <>
+      {transition.map(({ item, key, props }) => {
+        return item ? (
+          <animated.div
+            key={key}
+            className={classnames('lubycon-toast', className)}
+            style={{
+              ...style,
+              ...props,
+            }}
+            {...rest}
+          >
+            <ToastBody message={message} />
+          </animated.div>
+        ) : null;
+      })}
+    </>
   );
 };
 
--- ui-kit/src/stories/Toast.stories.tsx
+++ ui-kit/src/stories/Toast.stories.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import Toast from 'components/Toast';
 import Button from 'components/Button';
 import { useToast } from 'contexts/Toast';
+import { generateID } from 'src/utils';
 
 export default {
   title: 'Lubycon UI Kit/Toast',
@@ -15,22 +16,35 @@ export const Default = () => {
       <Toast show={true} message="데이터 전송이 완료되었습니다." />
       <Toast
         show={true}
-        message="사용하시는 단말기의 네트워크 상태가 좋지 않습니다.
-        \n
-        WIFI 혹은 3G/LTE 연결 상태를 확인해주세요."
+        message={`사용하시는 단말기의 네트워크 상태가 좋지 않습니다.\nWIFI 혹은 3G/LTE 연결 상태를 확인해주세요.`}
+      />
+    </div>
+  );
+};
+
+export const AutoHide = () => {
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      <Toast show={true} message="데이터 전송이 완료되었습니다." />
+      <Toast
+        show={show}
+        autoHideDuration={3000}
+        onHide={() => setShow(true)}
+        message={`사용하시는 단말기의 네트워크 상태가 좋지 않습니다.\nWIFI 혹은 3G/LTE 연결 상태를 확인해주세요.`}
       />
     </div>
   );
 };
 
 export const ToastHooks = () => {
-  const { open } = useToast();
+  const { openToast } = useToast();
   return (
     <div>
       <Button
         onClick={() =>
-          open({
-            message: '데이터 전송이 완료되었습니다',
+          openToast({
+            message: `데이터 전송이 완료되었습니다 - ${generateID('toast-test')}`,
           })
         }
       >
