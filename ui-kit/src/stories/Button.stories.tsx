8fe6ab7a11044139a6a1dab045f070d55ff28a4a
JunKim <93kimhyunjun@gmail.com>
2021-01-18 23:53:28 +0900

    feat(ui-kit): Button 컴포넌트 스토리 추가

--- ui-kit/src/stories/Button.stories.tsx
+++ ui-kit/src/stories/Button.stories.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
 import Button from 'components/Button';
+import Text from 'components/Text';
 import { Meta } from '@storybook/react/types-6-0';
 
 export default {
   title: 'Lubycon UI Kit/Button',
 } as Meta;
 
-const SIZE = ['Small', 'Medium', 'Large'];
-const STATUS = ['Normal', 'Hover', 'Active', 'Disabled'];
-const TEXT = '버튼 텍스트';
+const sizeList = ['small', 'medium', 'large'] as const;
+const btnText = '버튼 텍스트';
 
 export const Default = () => {
   return (
     <div>
-      <div>Rounded Button</div>
-      <Button>{TEXT}</Button>
+      <Text as="div" typography="h5" style={{ marginBottom: '40px' }}>
+        Rounded Button
+      </Text>
+      <ul style={{ listStyle: 'none' }}>
+        {sizeList.map((size, index) => (
+          <li
+            key={index}
+            style={{
+              display: 'grid',
+              gridTemplateColumns: '100px 150px 150px',
+              gridGap: '50px',
+              marginBottom: '30px',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ width: '100px' }}>{size.charAt(0).toUpperCase() + size.slice(1)}</Text>
+            <div>
+              <Button size={size} key={index}>
+                {btnText}
+              </Button>
+            </div>
+            <div>
+              <Button size={size} key={index + 'disabled'} disabled>
+                {btnText}
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
