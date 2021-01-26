e0f43a196e3cf96e79375a5eac1f0c4d14cc3922
evan-moon <bboydart91@gmail.com>
2021-01-26 16:03:14 +0900

    feat(ui-kit): 토스트에 Align 기능 추가

--- ui-kit/src/components/Toast/ToastBody.tsx
+++ ui-kit/src/components/Toast/ToastBody.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import classnames from 'classnames';
 import Text from 'components/Text';
 
+type ToastTextAlign = 'left' | 'center' | 'right';
+
 interface Props {
   message: string;
+  textAlign: ToastTextAlign;
 }
 
-const ToastBody = ({ message }: Props) => {
+const ToastBody = ({ message, textAlign = 'left' }: Props) => {
   return (
-    <div className={classnames('lubycon-toast--inbox', 'lubycon-shadow--3')}>
-      <Text typography="p2">{message}</Text>
+    <div
+      className={classnames(
+        'lubycon-toast__body',
+        'lubycon-shadow--3',
+        `lubycon-toast__body--align-${textAlign}`
+      )}
+    >
+      <Text typography="p2" className="lubycon-toast__text">
+        {message}
+      </Text>
     </div>
   );
 };
