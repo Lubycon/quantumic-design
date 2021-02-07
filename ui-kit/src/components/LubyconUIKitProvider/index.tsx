1bea9b14759edb7e7ed26d1bf555cb33a3f1b9ef
JunKim <93kimhyunjun@gmail.com>
2021-02-08 02:21:03 +0900

    feat(ui-kit): Modal TODO

--- ui-kit/src/components/LubyconUIKitProvider/index.tsx
+++ ui-kit/src/components/LubyconUIKitProvider/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { ToastProvider } from 'contexts/Toast';
 import { PortalProvider } from 'contexts/Portal';
+// import { ModalProvider } from 'contexts/Modal';
 
 interface Props {
   children: ReactNode;
@@ -10,6 +11,7 @@ function LubyconUIKitProvider({ children }: Props) {
   return (
     <PortalProvider>
       <ToastProvider>{children}</ToastProvider>
+      {/* <ModalProvider>{children}</ModalProvider> */}
     </PortalProvider>
   );
 }
