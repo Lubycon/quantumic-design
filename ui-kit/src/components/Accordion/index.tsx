a0e42ad7b0cbc1b3466f56be948cfac36de4af87
evan-moon <bboydart91@gmail.com>
2021-02-17 00:26:46 +0900

    feat(ui-kit): Accordion 컴포넌트 추가

---
+++ ui-kit/src/components/Accordion/index.tsx
@@ -0,0 +1,69 @@
+import React, { forwardRef, HTMLAttributes, useRef, useState } from 'react';
+import { Combine } from 'src/types/utils';
+import classnames from 'classnames';
+import Icon from '../Icon';
+import { chevronDown } from 'ionicons/icons';
+import Text from '../Text';
+import { useResizeObserver } from 'src/hooks/useResizeObserver';
+
+type Props = Combine<
+  {
+    label: string;
+    defaultOpen?: boolean;
+  },
+  HTMLAttributes<HTMLDivElement>
+>;
+const Accordion = forwardRef<HTMLDivElement, Props>(function Accordion(
+  { label, className, children, defaultOpen = false, ...props },
+  ref
+) {
+  const [open, setOpen] = useState(defaultOpen);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [bodyHeight, setBodyHeight] = useState(0);
+
+  const toggleContentOpen = () => {
+    setOpen((state) => !state);
+  };
+
+  const updateContentHeight = () =>
+    setBodyHeight(contentRef.current?.getBoundingClientRect().height ?? 0);
+
+  useResizeObserver(contentRef, updateContentHeight);
+
+  return (
+    <div
+      ref={ref}
+      className={classnames(
+        'lubycon-accordion',
+        {
+          'lubycon-accordion--opened': open,
+        },
+        className
+      )}
+      {...props}
+    >
+      <div className="lubycon-accordion__label" onClick={toggleContentOpen} role="button">
+        <Icon
+          icon={chevronDown}
+          type="outline"
+          size={20}
+          className="lubycon-accordion__label__icon"
+        />
+        <Text typography="subtitle" className="lubycon-accordion__label__text">
+          {label}
+        </Text>
+      </div>
+      <div className="lubycon-accordion__cover" style={{ height: open ? bodyHeight : 0 }}>
+        <div
+          className="lubycon-accordion__cover__content"
+          style={{ opacity: open ? 1 : 0 }}
+          ref={contentRef}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default Accordion;
