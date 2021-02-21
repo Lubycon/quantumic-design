3cc526682b29afcb125a8ce1b246de63eb1ae7e9
JunKim <93kimhyunjun@gmail.com>
2021-02-21 17:29:39 +0900

    chore: 모달 컴포넌트 스토리 수정

--- ui-kit/src/stories/Modal.stories.tsx
+++ ui-kit/src/stories/Modal.stories.tsx
@@ -1,8 +1,7 @@
 import { Meta } from '@storybook/react/types-6-0';
-import React from 'react';
-import Modal from 'components/Modal';
+import React, { useState } from 'react';
+import { Modal, ModalHeader, ModalContent, ModalFooter } from 'src';
 import Button from 'components/Button';
-import { useModal } from 'contexts/Modal';
 
 export default {
   title: 'Lubycon UI kit/Modal',
@@ -10,108 +9,21 @@ export default {
 } as Meta;
 
 export const Default = () => {
-  const { openModal } = useModal();
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(2, fit-content(100%))',
-        gridGap: '15px',
-      }}
-    >
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            title: '타이틀입니다',
-            message: '여기에 본문 텍스트가 들어갑니다\n' + '여기에 본문 텍스트가 들어갑니다',
-            cancelButton: true,
-          })
-        }
-      >
-        Small Modal Trigger (with all)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            message: '본문 텍스트와 타이틀은 용도에 따라 별도로 구성이 가능합니다',
-            cancelButton: true,
-          })
-        }
-      >
-        Small Modal Trigger (without title)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            title: '타이틀입니다',
-            message: '여기에 본문 텍스트가 들어갑니다\n' + '여기에 본문 텍스트가 들어갑니다',
-          })
-        }
-      >
-        Small Modal Trigger (without cancel)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            message: '본문 텍스트와 타이틀은 용도에 따라 별도로 구성이 가능합니다',
-          })
-        }
-      >
-        Small Modal Trigger (without title, cancel)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            title: '타이틀입니다',
-            message: `텍스트 내용이 많을 경우에는 중간 크기의 모달 사용을 권장합니다. 여기에 본문 텍스트를 입력해주세요.`,
-            size: 'medium',
-            cancelButton: true,
-          })
-        }
-      >
-        Medium Modal Trigger (with all)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            message: '본문 텍스트와 타이틀은 용도에 따라\n별도로 구성이 가능합니다',
-            size: 'medium',
-            cancelButton: true,
-          })
-        }
-      >
-        Medium Modal Trigger (without title)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            title: '타이틀입니다',
-            message: `텍스트 내용이 많을 경우에는 중간 크기의 모달 사용을 권장합니다. 여기에 본문 텍스트를 입력해주세요.`,
-            size: 'medium',
-          })
-        }
-      >
-        Medium Modal Trigger (without cancel)
-      </Button>
-      <Button
-        size="medium"
-        onClick={() =>
-          openModal({
-            message: '본문 텍스트와 타이틀은 용도에 따라\n별도로 구성이 가능합니다',
-            size: 'medium',
-          })
-        }
-      >
-        Medium Modal Trigger (without title, cancel)
-      </Button>
-    </div>
+    <>
+      <Button onClick={() => setShowModal(true)}>모달 열기</Button>
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        <ModalHeader title="타이틀입니다" />
+        <ModalContent>
+          <div>여기에 본문 텍스트가 들어갑니다</div>
+          <div>여기에 본문 텍스트가 들어갑니다</div>
+        </ModalContent>
+        <ModalFooter>
+          <Button size="small">저장하기</Button>
+        </ModalFooter>
+      </Modal>
+    </>
   );
 };
