466da73b86afe5755d06bd6bae105bebbb6e7619
JunKim <93kimhyunjun@gmail.com>
2021-02-28 01:06:30 +0900

    chore: 스토리 업데이트

--- ui-kit/src/stories/Modal.stories.tsx
+++ ui-kit/src/stories/Modal.stories.tsx
@@ -2,6 +2,7 @@ import { Meta } from '@storybook/react/types-6-0';
 import React, { useState } from 'react';
 import { Modal, ModalHeader, ModalContent, ModalFooter } from 'src';
 import Button from 'components/Button';
+import { colors } from 'constants/colors';
 
 export default {
   title: 'Lubycon UI kit/Modal',
@@ -11,17 +12,29 @@ export default {
 export const Default = () => {
   const [showModal, setShowModal] = useState(false);
 
+  const closeModal = () => setShowModal(false);
+  const handleOpen = () => console.info('open');
+
   return (
     <>
       <Button onClick={() => setShowModal(true)}>모달 열기</Button>
-      <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <ModalHeader title="타이틀입니다" />
-        <ModalContent>
+      <Modal show={showModal} onClose={closeModal} onOpen={handleOpen}>
+        <ModalHeader>타이틀입니다</ModalHeader>
+        <ModalContent className="Test">
           <div>여기에 본문 텍스트가 들어갑니다</div>
           <div>여기에 본문 텍스트가 들어갑니다</div>
         </ModalContent>
         <ModalFooter>
-          <Button size="small">저장하기</Button>
+          <Button
+            size="small"
+            style={{ color: colors.gray80, background: 'transparent', marginRight: '4px' }}
+            onClick={closeModal}
+          >
+            취소
+          </Button>
+          <Button size="small" onClick={closeModal}>
+            저장하기
+          </Button>
         </ModalFooter>
       </Modal>
     </>
