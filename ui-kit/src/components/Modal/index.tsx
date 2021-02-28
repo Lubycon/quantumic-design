e16beb07b093a8fab2d290e96fcac2156570f626
KIM HYUNJUN <93kimhyunjun@gmail.com>
2021-02-28 15:37:15 +0900

    feat(ui-kit): Modal 컴포넌트 추가 (#50)
    * feat(ui-kit): Modal 컴포넌트 추가
    
    * feat(ui-kit): Modal 스토리 추가
    
    * feat(ui-kit): Modal TODO
    
    * feat(ui-kit): Modal 컴포넌트 추가
    
    * feat(ui-kit): 타이틀 부분 태그 타입 수정
    
    * chore: 리뷰 내용 반영-백드롭 forwradRef
    
    * chore: 리뷰 내용 반영-컴포넌트 합성 방식으로 변경
    
    * chore: 모달 컴포넌트 스토리 수정
    
    * chore: 리뷰반영 - 타입 변경, 엘리먼트 유효성 검사
    
    * chore: 리뷰반영 - onOpen 이벤트, 백드롭 이벤트명
    
    * chore: 리뷰반영 - 컴포넌트 제네릭
    
    * chore: 린트 에러
    
    * chore: 스토리 업데이트

---
+++ ui-kit/src/components/Modal/index.tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement, cloneElement, useRef } from 'react';
+import ModalBackdrop from './ModalBackdrop';
+import ModalWindow from './ModalWindow';
+import classnames from 'classnames';
+import { generateID } from 'utils/index';
+import { useEffect } from 'react';
+
+export interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  show: boolean;
+  size?: 'small' | 'medium';
+  children: ReactElement[];
+  onOpen?: () => void;
+  onClose?: () => void;
+}
+
+const Modal = ({ show, size = 'small', children, onOpen, onClose }: ModalProps) => {
+  const backdropRef = useRef(null);
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (backdropRef.current == null) return;
+    if (event.target === backdropRef.current) onClose?.();
+  };
+
+  const onKeydown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') onClose?.();
+  };
+  useEffect(() => {
+    window.addEventListener('keydown', onKeydown);
+    return () => {
+      window.removeEventListener('keydown', onKeydown);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (show === true) {
+      onOpen?.();
+    }
+  }, [show]);
+
+  return show ? (
+    <div className={classnames('lubycon-modal')} tabIndex={-1} aria-hidden={true}>
+      <ModalBackdrop onClick={handleBackdropClick} ref={backdropRef} />
+      <ModalWindow size={size}>
+        {children.map((element) => {
+          return cloneElement(element, { key: generateID('lubycon-modal__children'), size: size });
+        })}
+      </ModalWindow>
+    </div>
+  ) : null;
+};
+
+export default Modal;
+export { default as ModalHeader } from './ModalHeader';
+export { default as ModalContent } from './ModalContent';
+export { default as ModalFooter } from './ModalFooter';
