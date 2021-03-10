c0176e60656c8759a472bcc0b254ca0cdd63a804
JunKim <93kimhyunjun@gmail.com>
2021-03-10 18:04:59 +0900

    chore: 컴포넌트, 스타일 import

--- ui-kit/src/index.ts
+++ ui-kit/src/index.ts
@@ -24,6 +24,13 @@ export { default as Input } from './components/Input';
 export { default as ProgressBar } from './components/ProgressBar';
 export { default as Accordion } from './components/Accordion';
 export { default as Modal, ModalHeader, ModalContent, ModalFooter } from './components/Modal';
+export {
+  default as Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from './components/Table';
 export { Portal } from './contexts/Portal';
 export { useToast } from './contexts/Toast';
 export { useSnackbar } from './contexts/Snackbar';
