2247045d4762515e58a7236fa0d2b2ee264dacf0
evan-moon <bboydart91@gmail.com>
2021-03-27 00:02:36 +0900

    feat(docs): Getting Started 추가

--- docs/src/components/PostSection/index.tsx
+++ docs/src/components/PostSection/index.tsx
@@ -2,13 +2,19 @@ import { colors, Text } from '@lubycon/ui-kit';
 import React, { PropsWithChildren, ReactNode } from 'react';
 
 const Title = ({ children }: PropsWithChildren<unknown>) => (
-  <Text typography="h5" fontWeight="bold" css={{ marginBottom: 24, display: 'block' }}>
+  <Text as="h2" typography="h5" fontWeight="bold" css={{ marginBottom: 24 }}>
+    {children}
+  </Text>
+);
+
+const Subtitle = ({ children }: PropsWithChildren<unknown>) => (
+  <Text as="h3" typography="h6" fontWeight="bold" css={{ marginBottom: 8 }}>
     {children}
   </Text>
 );
 
 const Contents = ({ children }: PropsWithChildren<unknown>) => (
-  <Text typography="h6" css={{ color: colors.gray80 }}>
+  <Text as="p" css={{ color: colors.gray80, marginBottom: 16 }}>
     {children}
   </Text>
 );
@@ -26,6 +32,7 @@ const PostSection = ({ title, children }: PropsWithChildren<Props>) => {
 };
 
 PostSection.Title = Title;
+PostSection.Subtitle = Subtitle;
 PostSection.Contents = Contents;
 
 export default PostSection;
--- docs/src/pages/HomePage/index.tsx
+++ docs/src/pages/HomePage/index.tsx
@@ -12,34 +12,25 @@ const HomePage = () => {
         </div>
         <div>
           <PostSection title={<PostSection.Title>Introduction</PostSection.Title>}>
-            Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
-            디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해 FE
-            라이브러리와 Figma 플러그인을 동시에 제공합니다.
+            <PostSection.Contents>
+              Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
+              디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해
+              FE 라이브러리와 Figma 플러그인을 동시에 제공합니다.
+            </PostSection.Contents>
           </PostSection>
           <PostSection title={<PostSection.Title>Guidelines</PostSection.Title>}>
-            Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
-            디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해 FE
-            라이브러리와 Figma 플러그인을 동시에 제공합니다.
+            <PostSection.Contents>
+              Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
+              디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해
+              FE 라이브러리와 Figma 플러그인을 동시에 제공합니다.
+            </PostSection.Contents>
           </PostSection>
           <PostSection title={<PostSection.Title>Links</PostSection.Title>}>
-            Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
-            디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해 FE
-            라이브러리와 Figma 플러그인을 동시에 제공합니다.
-          </PostSection>
-          <PostSection title={<PostSection.Title>Links</PostSection.Title>}>
-            Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
-            디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해 FE
-            라이브러리와 Figma 플러그인을 동시에 제공합니다.
-          </PostSection>
-          <PostSection title={<PostSection.Title>Links</PostSection.Title>}>
-            Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
-            디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해 FE
-            라이브러리와 Figma 플러그인을 동시에 제공합니다.
-          </PostSection>
-          <PostSection title={<PostSection.Title>Links</PostSection.Title>}>
-            Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
-            디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해 FE
-            라이브러리와 Figma 플러그인을 동시에 제공합니다.
+            <PostSection.Contents>
+              Lubycon UI Kit은 한국의 고유 문자인 한글을 기반으로 설계된 UI 라이브러리입니다. 국내
+              디자이너와 개발자들이 동일한 도메인 언어로 커뮤니케이션하는 것을 지향하며, 이를 위해
+              FE 라이브러리와 Figma 플러그인을 동시에 제공합니다.
+            </PostSection.Contents>
           </PostSection>
         </div>
       </div>
