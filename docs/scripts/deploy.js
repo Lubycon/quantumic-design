7471afa5220008339701fac3c7f9a389f5998288
Evan Moon <bboydart91@gmail.com>
2021-03-27 14:31:44 +0900

    chore(docs): static resource 404 떨어지는 이슈 해결 (#64)
    * fix(docs): .nojekyll 파일 생성
    
    * fix(docs): .nojekyll 파일 재생성
    
    * fix(docs): 배포 테스트
    
    * chore(docs): 배포 테스트
    
    * chore(docs): 배포 테스트
    
    * CI/CD 타겟 브랜치 수정

--- docs/scripts/deploy.js
+++ docs/scripts/deploy.js
@@ -6,10 +6,11 @@ const env = process.env.ENV;
 const token = process.env.ACCESS_TOKEN;
 const deployTarget = env === 'alpha' ? 'ui-kit.alpha.lubycon.io' : 'ui-kit.lubycon.io';
 
-console.log('📦 UI Kit 문서 알파 배포를 준비 중 입니다...');
+console.log('📦 UI Kit 문서 배포를 준비 중 입니다...');
 
 console.log('🌱 CNAME 만드는 중...');
 fs.renameSync(path.resolve(`./CNAME.${env}`), path.resolve('./out/CNAME'));
+fs.closeSync(fs.openSync(path.resolve('./out/.nojekyll'), 'w'));
 console.log('🌱 CNAME 생성 완료');
 
 ghpages.publish(
@@ -19,12 +20,13 @@ ghpages.publish(
     remote: 'origin',
     repo: `https://${token}@github.com/Lubycon/${deployTarget}`,
     message: `UI Kit 문서 배포`,
+    dotfiles: true,
   },
   (err) => {
     if (err) {
       throw err;
     } else {
-      console.log('🚀 UI Kit 문서 알파 배포가 완료되었습니다!');
+      console.log('🚀 UI Kit 문서 배포가 완료되었습니다!');
     }
   }
 );
