cf3cac5ae14518987d009964d3d14b8d6bce861e
JunKim <93kimhyunjun@gmail.com>
2021-04-09 01:14:48 +0900

    chore(docs): highlight.js 컴포넌트 추가 + 기존 페이지 적용

--- docs/src/pages/GettingStartedPage/index.tsx
+++ docs/src/pages/GettingStartedPage/index.tsx
@@ -1,5 +1,6 @@
 import BasicLayout from 'components/BasicLayout';
 import PostSection from 'components/PostSection';
+import Highlight from 'components/CodeHighlight';
 import React from 'react';
 
 const GettingStartedPage = () => {
@@ -11,18 +12,18 @@ const GettingStartedPage = () => {
             Lubycon UI Kit은 아직 알파 버전만 배포된 상태입니다. 따라서 문서 상단의 버전을 확인하고
             latest가 아닌 정확한 버전을 명시하여 설치하시는 것을 추천합니다.
           </PostSection.Contents>
-          <pre>
+          <Highlight lang="bash">
             {`$ npm install @lubycon/ui-kit@v1.1.0-alpha.24
 // or
 $ yarn add @lubycon/ui-kit@v1.1.0-alpha.24`}
-          </pre>
+          </Highlight>
         </PostSection>
         <PostSection title={<PostSection.Title>Usage</PostSection.Title>}>
           <PostSection.Contents>
             Lubycon UI Kit 내부의 몇몇 컴포넌트는 컴포넌트 트리와 분리된 상태와 렌더 트리를 가지고
             있기 때문에 LubyconUIKitProvider을 필요로 합니다.
           </PostSection.Contents>
-          <pre>
+          <Highlight lang="ts">
             {`// App.tsx
 
 import React, { PropsWithChildren } from 'react';
@@ -31,7 +32,7 @@ import { LubyconUIKitProvider } from '@lubycon/ui-kit';
 function App({ children }: PropsWithChildren<{}>) {
   return <LubyconUIKitProvider>{children}</LubyconUIKitProvider>;
 }`}
-          </pre>
+          </Highlight>
         </PostSection>
         <PostSection title={<PostSection.Title>선언적 렌더링 vs Hooks</PostSection.Title>}>
           <PostSection.Contents>
@@ -42,7 +43,7 @@ function App({ children }: PropsWithChildren<{}>) {
             선언적인 방법과 명령적인 방법을 선택해서 사용할 수 있도록 지원하고 있습니다.
           </PostSection.Contents>
           <PostSection.Subtitle>선언적 렌더링</PostSection.Subtitle>
-          <pre>
+          <Highlight lang="ts">
             {`import React, { useState } from 'react';
 import { Toast } from '@lubycon/ui-kit';
 
@@ -58,9 +59,9 @@ function Foo() {
 }
 
 export default Foo;`}
-          </pre>
+          </Highlight>
           <PostSection.Subtitle>Hooks</PostSection.Subtitle>
-          <pre>
+          <Highlight lang="ts">
             {`import React from 'react';
 import { useToast, Button } from '@lubycon/ui-kit';
 
@@ -79,7 +80,7 @@ function Foo() {
 }
 
 export default Foo;`}
-          </pre>
+          </Highlight>
         </PostSection>
       </div>
     </BasicLayout>
