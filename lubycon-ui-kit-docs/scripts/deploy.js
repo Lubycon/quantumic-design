0d2fce72eadd854458a02493701907ac6a8aecd9
Evan Moon <bboydart91@gmail.com>
2021-04-11 18:05:05 +0900

    fix(ui-kit): CSS Export 설정 변경 (#73)
    * chore(docs): 배포 테스트
    
    * fix(ui-kit): CSS Export 설정 변경

--- lubycon-ui-kit-docs/scripts/deploy.js
+++ lubycon-ui-kit-docs/scripts/deploy.js
@@ -2,31 +2,32 @@ const ghpages = require('gh-pages');
 const path = require('path');
 const fs = require('fs');
 
-console.log(path.resolve(`./CNAME.alpha`));
 const env = process.env.ENV;
+
 const token = process.env.ACCESS_TOKEN;
 const deployTarget = env === 'alpha' ? 'ui-kit.alpha.lubycon.io' : 'ui-kit.lubycon.io';
 
 console.log('📦 UI Kit 문서 배포를 준비 중 입니다...');
 
 console.log('🌱 CNAME 만드는 중...');
+console.log(path.resolve(`./CNAME.${env}`));
 fs.renameSync(path.resolve(`./CNAME.${env}`), path.resolve('./public/CNAME'));
 console.log('🌱 CNAME 생성 완료');
 
-// ghpages.publish(
-//   path.join(__dirname, '../public'),
-//   {
-//     branch: 'master',
-//     remote: 'origin',
-//     repo: `https://${token}@github.com/Lubycon/${deployTarget}`,
-//     message: `UI Kit 문서 배포`,
-//     dotfiles: true,
-//   },
-//   (err) => {
-//     if (err) {
-//       throw err;
-//     } else {
-//       console.log('🚀 UI Kit 문서 배포가 완료되었습니다!');
-//     }
-//   }
-// );
+ghpages.publish(
+  path.join(__dirname, '../public'),
+  {
+    branch: 'master',
+    remote: 'origin',
+    repo: `https://${token}@github.com/Lubycon/${deployTarget}`,
+    message: `UI Kit 문서 배포`,
+    dotfiles: true,
+  },
+  err => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('🚀 UI Kit 문서 배포가 완료되었습니다!');
+    }
+  }
+);
