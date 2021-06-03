fe358d8100370cc82f03258286bb0fadaccd86c1
JunKim <93kimhyunjun@gmail.com>
2021-06-04 00:49:40 +0900

    chore(ui-kit): 스토리북-코드샌드박스 라이브러리 추가
    - 임시적으로 스토리북 고도화 하기 위한 목적

--- ui-kit/.storybook/main.js
+++ ui-kit/.storybook/main.js
@@ -1,10 +1,10 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { resolve } = require('path');
+const codesandbox = require('remark-codesandbox');
+const codesandboxTemplatePackageJSON = require('../src/template/codesandbox/package.json');
 
 module.exports = {
-  stories: [
-    '../src/**/*.stories.(ts|tsx|mdx)',
-    '../src/stories/**/*.mdx',
-  ],
+  stories: ['../src/**/*.stories.(ts|tsx|mdx)', '../src/stories/**/*.mdx'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
@@ -15,6 +15,42 @@ module.exports = {
   ],
   webpackFinal: async (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+
+    const mdxRule = config.module.rules.find((rule) => rule.test?.test('.story.mdx'));
+    const {
+      options: { remarkPlugins },
+    } = mdxRule.use.find(({ loader }) => loader === require.resolve('@mdx-js/loader'));
+
+    remarkPlugins.push([
+      codesandbox,
+      {
+        mode: 'iframe',
+        query: {
+          fontsize: 14,
+        },
+        customTemplates: {
+          lubycon: {
+            extends: `file:${resolve(__dirname, '../src/template/codesandbox')}`,
+            entry: 'src/App.tsx',
+          },
+          'lubycon-map': {
+            extends: 'lubycon',
+            files: {
+              'package.json': {
+                content: {
+                  ...codesandboxTemplatePackageJSON,
+                  dependencies: {
+                    ...codesandboxTemplatePackageJSON.dependencies,
+                  },
+                },
+              },
+            },
+          },
+        },
+        autoDeploy: process.env.NODE_ENV === 'production',
+      },
+    ]);
+
     return config;
   },
-}
+};
