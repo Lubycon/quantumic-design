a33327d82a74e0ebf16f4fcb791d289877bd7336
Evan Moon <bboydart91@gmail.com>
2021-06-05 11:50:24 +0900

    docs: Snackbar, Switch 등 5개 컴포넌트 스토리 마이그레이션 (#87)
    * docs: 스낵바 마이그레이션
    
    * docs: Switch 마이그레이션
    
    * docs: Table 마이그레이션
    
    * docs: Tabs 마이그레이션
    
    * docs: Tag 마이그레이션

---
+++ ui-kit/src/stories/Components/Table/data.ts
@@ -0,0 +1,3 @@
+export const header = ['제목', '제목', '제목', '제목', '제목'];
+export const contents = ['내용', '내용을 입력하세요', '내용', '내용을 입력하세요', '내용'];
+export const iterator = [undefined, undefined, undefined, undefined, undefined, undefined];
