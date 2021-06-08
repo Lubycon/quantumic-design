38ab2b91bfd101e7c1bc65908c7d54fe55929ac5
evan-moon <bboydart91@gmail.com>
2021-06-09 00:10:58 +0900

    docs: tooltip, text 컴포넌트 mdx 마이그레이션

---
+++ ui-kit/src/stories/Components/Tooltip/data.ts
@@ -0,0 +1,12 @@
+import { TooltipPosition } from 'src/components/Tooltip/types';
+
+export const positions: TooltipPosition[] = [
+  'top-left',
+  'top-center',
+  'top-right',
+  'left',
+  'right',
+  'bottom-left',
+  'bottom-center',
+  'bottom-right',
+];
