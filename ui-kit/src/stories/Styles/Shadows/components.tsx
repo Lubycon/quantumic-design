65a92f29ff902b2a87087b1b6c4ffaa15aece5a1
evan-moon <bboydart91@gmail.com>
2021-06-26 23:45:42 +0900

    docs: Shadow 문서 업데이트

---
+++ ui-kit/src/stories/Styles/Shadows/components.tsx
@@ -0,0 +1,19 @@
+import React, { ComponentProps } from 'react';
+
+export const Box = ({ children, ...rest }: ComponentProps<'div'>) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'flex-end',
+        height: 80,
+        borderRadius: 8,
+        backgroundColor: '#fcfcfd',
+        padding: 34,
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
--- ui-kit/src/stories/Styles/Shadows/data.ts
+++
@@ -1,8 +0,0 @@
-export const shadowTypes = [
-  '0px',
-  '2px 드랍다운',
-  '3px 버튼, 카드',
-  '6px 토스트',
-  '8px 탭',
-  '24px 모달 팝업',
-];
