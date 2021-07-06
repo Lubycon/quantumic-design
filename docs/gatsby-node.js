76b3bc4e70a3db7efeec50fb8f692296564276f2
evan-moon <bboydart91@gmail.com>
2021-07-06 23:06:14 +0900

    모노레포 제거

--- docs/gatsby-node.js
+++
@@ -1,5 +0,0 @@
-const { createPages } = require('./gatsby-hooks/createPages');
-const { onCreateNode } = require('./gatsby-hooks/onCreateNode');
-
-exports.createPages = createPages;
-exports.onCreateNode = onCreateNode;
