b09a994813baaaddc87622955e6fa3d032db0f7b
Evan Moon <bboydart91@gmail.com>
2021-07-22 12:29:27 +0900

    refactor: Emotion 추가 및 담당 컴포넌트의 스타일 제거 (#101)
    * refactor(ui-kit): 사용하지 않는 컴포넌트 제거
    
    * refactor(ui-kit): emotion 셋업
    
    * remove unused rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,12 +19,14 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
+  plugins: ['@emotion'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
   },
 };
---
+++ src/hooks/useProgress.ts
@@ -0,0 +1,31 @@
+interface ProgressOptions {
+  min: number;
+  value: number;
+  valueMapper?: (value: number) => number;
+  max: number;
+}
+
+/**
+ * value와 max간 퍼센테이지를 계산합니다. valueMapper 함수를 사용하여 value의 상승폭을 변경할 수 있습니다.
+ *
+ * ex)
+ * useProgress({
+ *   min: 0,
+ *   max: 100,
+ *   value: 50,
+ *   valueMapper: (value: number) => BezierEasing(0.08, 0.4, 0.5, 0.82)
+ * })
+ */
+function useProgress({ min = 0, value: originValue, valueMapper, max }: ProgressOptions) {
+  const value = valueMapper?.(originValue) ?? originValue;
+
+  if (value < min) {
+    return 0;
+  } else if (value > max) {
+    return 100;
+  }
+
+  return (value / max) * 100;
+}
+
+export default useProgress;
