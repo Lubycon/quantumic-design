61fea9cfb3694f8195159068053dbb255162a212
HyeIn Ju <hyein.ju92@gmail.com>
2021-07-28 00:41:21 +0900

    refactor: Tag 컴포넌트 삭제

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ export {
 export { default as Snackbar } from './components/Snackbar';
 export { default as Input } from './components/Input';
 export { default as Accordion } from './components/Accordion';
-export { default as Tag } from './components/Tag';
 export { Portal } from './contexts/Portal';
 export { useSnackbar } from './contexts/Snackbar';
 export { colors } from './constants/colors';
