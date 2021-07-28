a8bb24423ddca8992841b18192166ca02ed5fbdb
HyeIn Ju <hyein.ju92@gmail.com>
2021-07-28 14:47:56 +0900

    refactor: 스타일 제거 시 의미 없는 컴포넌트 제거  (#104)
    * refactor: Switch 컴포넌트 삭제
    
    * refactor: Table 컴포넌트 삭제
    
    * refactor: Tabs 컴포넌트 삭제
    
    * refactor: Tag 컴포넌트 삭제

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,9 @@ export { default as Button } from './components/Button';
 export { default as Checkbox } from './components/Checkbox';
 export { Row, Column } from './components/Grid';
 export { default as Container } from './components/Container';
-export { default as Switch } from './components/Switch';
 export { default as Text } from './components/Text';
 export { default as LubyconUIKitProvider } from './components/LubyconUIKitProvider';
 export { default as Tooltip } from './components/Tooltip';
-export { default as Tabs, TabsItem } from './components/Tabs';
 export {
   default as Card,
   CardHeader,
@@ -18,8 +16,6 @@ export {
 export { default as Snackbar } from './components/Snackbar';
 export { default as Input } from './components/Input';
 export { default as Accordion } from './components/Accordion';
-export { default as Tag } from './components/Tag';
-export { default as Table, TableHead, TableBody, TableRow, TableCell } from './components/Table';
 export { Portal } from './contexts/Portal';
 export { useSnackbar } from './contexts/Snackbar';
 export { colors } from './constants/colors';
