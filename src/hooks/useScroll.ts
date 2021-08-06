58cf3fbee6eec7990705932cb08bd1d9b434fa9d
HyeIn Ju <hyein.ju92@gmail.com>
2021-08-06 21:47:22 +0900

    Chore: 성능이슈발생 여지에 대한 부분jsDoc 추가

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,5 +1,11 @@
 import { RefObject, useEffect } from 'react';
 
+/**
+ * 컴포넌트에 스크롤 이벤트를 추가하는 hook입니다.
+ * useEffect dependency로 hook의 인자인 ref, scrollCallback을 포함하고 있어
+ * ref, scrollCallback의 레퍼런스가 변경 될 때마다 스크롤 이벤트가 바인딩됩니다.
+ * 이 부분을 참고하셔서 성능이슈가 발생하지 않도록 잘 관리해주세요.
+ */
 const useScroll = (ref: RefObject<HTMLElement>, scrollCallback: () => void) => {
   useEffect(() => {
     if (ref.current === null) {
