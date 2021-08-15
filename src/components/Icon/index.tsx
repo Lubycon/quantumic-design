369808dfbadad73ed8b111b369b51ecf16dd13f3
Evan Moon <bboydart91@gmail.com>
2021-08-15 21:29:22 +0900

    fix(Icon): 아이콘이 깨지는 이슈 수정 (#119)

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -25,7 +25,7 @@ type Props = CombineElementProps<
 const Icon = ({
   name,
   size = 16,
-  type: propsType = 'outline',
+  type: propsType = 'filled',
   color = colors.gray100,
   className,
   ...rest
@@ -63,14 +63,7 @@ const Icon = ({
 
   return (
     <span
-      className={classnames(
-        'lubycon-icon',
-        {
-          'lubycon-icon--outline': type === 'outline',
-          'lubycon-icon--filled': type === 'filled',
-        },
-        className
-      )}
+      className={classnames('lubycon-icon', className)}
       style={{ width: size, height: size }}
       {...rest}
     >
