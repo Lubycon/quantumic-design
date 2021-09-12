bdf975aba59554d0615e38bf3b5bc0020d22dc97
evan-moon <bboydart91@gmail.com>
2021-09-12 12:29:17 +0900

    fix(Text): 폰트 웨이트 안 먹던 이슈 수정

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,9 +2,10 @@ import { CSSProperties } from '@emotion/react/node_modules/@emotion/serialize';
 import { ElementType, Ref, forwardRef } from 'react';
 import { OverridableProps } from '../../types/OverridableProps';
 
-export const DEFAULT_ELEMENT = 'span' as const;
+export const DEFAULT_ELEMENT = 'p' as const;
 
 interface TextBaseProps {
+  display?: CSSProperties['display'];
   lineHeight?: CSSProperties['lineHeight'];
   weight?: CSSProperties['fontWeight'];
   size?: CSSProperties['fontSize'];
@@ -14,17 +15,19 @@ interface TextBaseProps {
 type TextProps<T extends ElementType = typeof DEFAULT_ELEMENT> = OverridableProps<T, TextBaseProps>;
 
 const Text = <T extends ElementType = typeof DEFAULT_ELEMENT>(
-  { lineHeight, fontWeight, size, color, align, as, children, ...props }: TextProps<T>,
+  { display, lineHeight, weight, size, color, align, as, children, ...props }: TextProps<T>,
   ref: Ref<any>
 ) => {
   const target = as ?? DEFAULT_ELEMENT;
   const Component = target;
+
   return (
     <Component
       ref={ref}
       role="text"
       css={{
-        fontWeight,
+        display,
+        fontWeight: weight,
         lineHeight,
         fontSize: size,
         color,
