56bb48dc6842c3b1f85dbd50d0d1c2c8c458dcdd
evan-moon <bboydart91@gmail.com>
2021-10-10 18:32:53 +0900

    refactor: remove react-spring

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export { default as Shadow } from './components/Shadow';
 export { default as Spacing } from './components/Spacing';
 export { default as Skeleton } from './components/Skeleton';
 export { useOverlay } from './contexts/Overlay';
-export { default as TransitionMotion } from './components/TransitionMotion';
 export { default as useElementSize } from './hooks/useElementSize';
 export { default as Flex } from './components/Flex';
 export { CombineElementProps, Combine } from './types/utils';
