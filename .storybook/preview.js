fe052bbdebf9c3ca23e562aa5db7fd26d38a150d
evan-moon <bboydart91@gmail.com>
2022-11-10 23:58:12 +0900

    더 이상 quantumic-design의 관심사가 아닌 useOverlay 스토리 제거

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { OverlayProvider } from '@lubycon/react';
 import { QDProvider, Container } from 'src';
 
-export const decorators = [(Story => (
-  <QDProvider>
-    <Container>
-      <Story />
-    </Container>
-  </QDProvider>
-))];
+export const decorators = [
+  (Story) => (
+    <OverlayProvider>
+      <QDProvider>
+        <Container>
+          <Story />
+        </Container>
+      </QDProvider>
+    </OverlayProvider>
+  ),
+];
 
 export const parameters = {
   options: {
     storySort: {
       method: '',
-      order: ['Quantumic Design',['Welcome', 'Getting Started', 'Usage'], 'Styles', 'Components'], 
-      locales: '', 
+      order: ['Quantumic Design', ['Welcome', 'Getting Started', 'Usage'], 'Styles', 'Components'],
+      locales: '',
     },
   },
 };
